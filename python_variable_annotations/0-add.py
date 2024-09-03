324be7842da6aaf1d06deeded260a1557ec15d11
root <n.uwayo@alustudent.com>
2024-09-03 16:55:33 +0000

    annotations

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+""" function to add two float numbers
+"""
+
+
+def add(a: float, b: float) -> float:
+    """ Adds two float numbers and return the result.
+    """
+    return a + b
