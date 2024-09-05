96a0832139bf2dd1a0681767a588eb850b46edb6
root <n.uwayo@alustudent.com>
2024-09-05 09:39:13 +0000

    1

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -5,10 +5,10 @@ Async Comprehensions.
 
 import asyncio
 from typing import List
-from 0_basic_async_syntax import wait_random  # Standard import
+from basic_async_syntax import wait_random  # Corrected import
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """ Return the 10 random numbers."""
+    """ Return the 10 random numbers. """
     tasks = [asyncio.create_task(wait_random(max_delay)) for _ in range(n)]
     return [await task for task in asyncio.as_completed(tasks)]
 
