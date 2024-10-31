e728ee914abb4e2c1ec056a25cbbde8cef841b16
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2024-10-31 22:30:41 +0200

    loop

--- ES6_basic/10-loops.js
+++ ES6_basic/10-loops.js
@@ -1,8 +1,7 @@
 export default function appendToEachArrayValue(array, appendString) {
-  const res = [];
-  for (const value of array) {
-    res.append(appendString + value);
+  for (const [index, value] of array.entries()) {
+    array[index] = appendString + value;
   }
 
-  return res;
+  return array;
 }
