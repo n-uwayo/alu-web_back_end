db7bd997652a193306c83030a5bf54cbc68c4198
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2024-10-31 22:13:17 +0200

    block-scoped

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,8 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const task = true;
-    const task2 = false;
+    let task = true; // `let` makes this block-scoped
+    let task2 = false; // `let` makes this block-scoped
   }
 
   return [task, task2];
