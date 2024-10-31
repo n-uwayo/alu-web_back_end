e81b723698b41f2b59da24b930972e7dc5a5b79a
n-uwayo <122350054+n-uwayo@users.noreply.github.com>
2024-10-31 22:18:01 +0200

    block-scoped

--- ES6_basic/1-block-scoped.js
+++ ES6_basic/1-block-scoped.js
@@ -3,9 +3,6 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    const innerTask = true; // Renaming to avoid conflicting with outer scope
-    const innerTask2 = false; // Renaming to avoid conflicting with outer scope
-    // innerTask and innerTask2 are not used further
   }
 
   return [task, task2];
